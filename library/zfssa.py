ac0b9738d8de3351077538ba134bcb7641b9fe54
amaura <amaury.francois@gmail.com>
2016-12-13 00:40:14 +0100

    Fields

--- library/zfssa.py
+++ library/zfssa.py
@@ -14,11 +14,15 @@ short_description: Create projects and shares on Oracle ZFS Appliance
 from ansible.module_utils.basic import *
 
 def main():
+    fields = {
+        "hostname": {"required": True, "type": "str"},
+        },
+    }
 
     module = AnsibleModule(argument_spec={})
     response = {"hello": "world"}
     module.exit_json(changed=False, meta=response)
 
 
-if __name__ == '__main__':  
+if __name__ == '__main__':
     main()
