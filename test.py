22c27d12e42895f1a1eba5737c4716bb02f0cde4
amaura <amaury.francois@gmail.com>
2016-12-13 01:17:44 +0100

    Test API Call

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-zfs_user='root'
-zfs_password='welcome1'
-
-url="https://192.168.59.103:215/api/access/v1"
-response=requests.get(url, auth=('zfs_username', 'zfs_password'),verify=False)
-
-print response.json()
