1bf6b816a5e4bb55b89b8502e718ec9d4e8249a4
amaura <amaury.francois@gmail.com>
2016-12-13 01:16:58 +0100

    Test API Call

--- library/zfssa.py
+++ library/zfssa.py
@@ -10,18 +10,30 @@ module: zfssa
 short_description: Create projects and shares on Oracle ZFS Appliance
 '''
 
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 from ansible.module_utils.basic import *
 
+
 def main():
+    zfs_user='root'
+    zfs_password='welcome1'
+
     fields = {
         "hostname": {"required": True, "type": "str"},
         }
 
 
+
     module = AnsibleModule(argument_spec=fields)
-    response = {"hello": "world"}
-    module.exit_json(changed=False, meta=response)
+
+    url="https://"+module.params['hostname']+":215/api/access/v1"
+    response=requests.get(url, auth=('zfs_username', 'zfs_password'),verify=False)
+
+
+    module.exit_json(changed=False, meta=response.json())
 
 
 if __name__ == '__main__':
